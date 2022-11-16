e60c49c6ee1694beff3d6b66fd89bff23bf01a4b
Matthieu Hog <matthieu.hog@mikrosimage.com>
2022-11-16 10:03:17 +0100

    added bash script to run benchmark with CLI

---
+++ scripts/run_benchmark.sh
@@ -0,0 +1,23 @@
+echo "Hello"
+
+#rez env meshroom-develop mrrs colmap
+
+#input_folder=/s/prods/mvg/_source_global/users/hogm/datasets/blendedMVSNEt/debug_set/
+#output_folder=/s/prods/mvg/_source_global/users/hogm/meshroom_benchmark_outputs_test
+
+input_folder=/s/prods/mvg/_source_global/users/hogm/datasets/blendedMVSNEt/low_res/BlendedMVS
+output_folder=/s/prods/mvg/_source_global/users/hogm/meshroom_benchmark_outputs
+
+#submit="-s"
+submit=""
+
+
+#python ../clis/benchmark.py run $input_folder -o $output_folder  -r -c -s -u BlendedMVGDataset_1
+
+#branch gt
+#python ../clis/benchmark.py run $input_folder -o $output_folder  -r -c -s -u Texturing_2
+
+#python ../clis/benchmark.py run $input_folder -o $output_folder -r -c -s -u Texturing_4
+#python ../clis/benchmark.py run $input_folder -o $output_folder -r -c -s -u Texturing_6
+
+python ../clis/benchmark.py run $input_folder -o $output_folder -r -c $submit -u CalibrationComparison_2
