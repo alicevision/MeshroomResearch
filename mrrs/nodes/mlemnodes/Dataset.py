5e4963094aaee8bbfc9cb94402fe42ff18f9c4e8
mh0g <matthieu.hog@gmail.com>
2022-12-09 16:53:31 +0100

    actually writting meta for gregoire

--- mrrs/nodes/mlemnodes/Dataset.py
+++ mrrs/nodes/mlemnodes/Dataset.py
@@ -294,9 +294,10 @@ class Dataset(desc.Node):
                 #compute projection matrices for meshroom
                 camera_center = gt_extrinsic[0:3,3]
                 inverse_intr_rot = np.linalg.inv(gt_intrinsic@np.linalg.inv(gt_extrinsic[0:3,0:3]))
-                depth_meta = {"AliceVision:CArr":camera_center,
-                              "AliceVision:iCamArr":inverse_intr_rot, "AliceVision:downscale":1} #TOTEST
-
+                # depth_meta = {"AliceVision:CArr":camera_center,
+                            #   "AliceVision:iCamArr":inverse_intr_rot, "AliceVision:downscale":1} #TOTEST
+                custom_header={}
+                custom_header["worldtocamera"] = (1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)
                 #save exr
                 save_exr(depth_map_gt, os.path.join(chunk.node.outputGroundTruthdepthMapsFolder.value, str(view_id)+"_depthMap.exr"),#FIXME: harcoded names
                         data_type="depth", custom_header=depth_meta)
