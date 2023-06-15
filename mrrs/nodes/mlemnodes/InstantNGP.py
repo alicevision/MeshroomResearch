e5156e47ce95c30395c1bbd32e44fb05ad24db61
mh0g <matthieu.hog@gmail.com>
2023-06-15 17:26:57 +0200

    transformed the ngp node into a cli/conda node

--- mrrs/nodes/mlemnodes/InstantNGP.py
+++ mrrs/nodes/mlemnodes/InstantNGP.py
@@ -4,14 +4,9 @@ This node launches instant-ngp.
 __version__ = "1.0"
 
 import os
-import json
-import re
-import cv2
 
 from meshroom.core import desc
-from mrrs.core.geometry import *
-from mrrs.core.utils import *
-from mrrs.core.ios import *
+from mrrs.core.CondaNode import CondaNode
 from mrrs.implicit_mesh.instant_ngp import ENV_FILE
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
@@ -19,27 +14,28 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 CONDA_ENV_PATH = "/home/bbrument/anaconda3/envs/instant-ngp/bin"
 NGP_COMMAND = f"{CONDA_ENV_PATH}/python "+ os.path.join(os.path.dirname(__file__), "../../implicit_mesh/instant_ngp/instant-ngp/scripts/run.py")#FIXME: hardcoded
 MODEL = "instant-ngp/configs/nerf/base.json"
-MODEL_PATH = os.path.join(os.path.dirname(__file__),"../../implicit_mesh/instant_ngp",MODEL)#FIXME: hardcoded
+# MODEL_PATH = os.path.join(os.path.dirname(__file__),"../../implicit_mesh/instant_ngp",MODEL)#FIXME: hardcoded
 
 
-class InstantNGP(desc.Node):
+class InstantNGP(CondaNode):
     """
     This node launches InstantNGP.
     """
-
-    #overides the env path
-    @property
-    def env_file(self):
-        return ENV_FILE
-    
     category = 'Meshroom Research'
     gpu = desc.Level.INTENSIVE
     documentation = '''Util node to launch instant-ngp.'''
 
+     #overides the env path
+    @property
+    def env_file(self):
+        return ENV_FILE
+
+    commandLine = NGP_COMMAND
+
     inputs = [
 
         desc.File(
-            name="inputNerfData",
+            name="training_data",
             label="Input NeRF Data",
             description="Input NeRF Data",
             value="",
@@ -47,7 +43,7 @@ class InstantNGP(desc.Node):
         ),
 
         desc.IntParam(
-            name='marchingCubesRes',
+            name='marching_cubes_res',
             label='Marching Cubes Resolution',
             description='Marching Cubes Resolution',
             value=256,
@@ -56,7 +52,7 @@ class InstantNGP(desc.Node):
         ),
 
         desc.IntParam(
-            name='nSteps',
+            name='n_steps',
             label='Number of steps',
             description='Number of iterations for the training.',
             value=30000,
@@ -77,49 +73,36 @@ class InstantNGP(desc.Node):
 
     outputs = [
         desc.File(
-            name='outputMesh',
+            name='save_mesh',
             label='Output mesh',
             description='Output mesh.',
             value=desc.Node.internalFolder + 'mesh.obj',
             uid=[],
+            group=''
         ),
     ]
 
-    def check_inputs(self, chunk):
-        """
-        Checks that all inputs are properly set.
-        """
-        if chunk.node.inputNerfData.value == '':
-            chunk.logger.warning(
-                'No input NeRF data in node InstantNGP, skipping')
-            return False
-        return True
 
-    def processChunk(self, chunk):
-        """
-        Launches InstantNGP.
-        """
-        try:
-            chunk.logManager.start(chunk.node.verboseLevel.value)
-            if not self.check_inputs(chunk):
-                return
-            
-            # Launch instant-ngp
-            cmd = NGP_COMMAND + (f" --mode nerf"
-                                +f" --training_data {chunk.node.inputNerfData.value}"
-                                +f" --marching_cubes_res {chunk.node.marchingCubesRes.value}"
-                                +f" --save_mesh {chunk.node.outputMesh.value}"
-                                +f" --network {MODEL_PATH}"
-                                +f" --n_steps {chunk.node.nSteps.value}")
-            print(cmd)
-            do_system(cmd)            
+    # def processChunk(self, chunk):
+    #     """
+    #     Launches InstantNGP.
+    #     """
+    #     try:
+    #         chunk.logManager.start(chunk.node.verboseLevel.value)
+    #         if not self.check_inputs(chunk):
+    #             return
 
-            chunk.logger.info('InstantNGP done.')
-        finally:
-            chunk.logManager.end()
+    #         # Launch instant-ngp
+    #         cmd = NGP_COMMAND + (f" --mode nerf"
+    #                             +f" --training_data {chunk.node.inputNerfData.value}"
+    #                             +f" --marching_cubes_res {chunk.node.marchingCubesRes.value}"
+    #                             +f" --save_mesh {chunk.node.outputMesh.value}"
+    #                             +f" --network {MODEL_PATH}"
+    #                             +f" --n_steps {chunk.node.nSteps.value}")
+    #         print(cmd)
+    #         do_system(cmd)
 
-
-        
-
-        return True
+    #         chunk.logger.info('InstantNGP done.')
+    #     finally:
+    #         chunk.logManager.end()
 
