502c10c90bdf23c8e4777e9278024366d1a337f6
mh0g <matthieu.hog@gmail.com>
2023-06-19 16:23:10 +0200

    removed hardcoded env

--- mrrs/nodes/colmap/__init__.py
+++ mrrs/nodes/colmap/__init__.py
@@ -2,7 +2,7 @@ from sys import platform
 
 COLMAP=""
 if platform == "linux" or platform == "linux2":
-    COLMAP="rez env colmap-faca -- colmap"#FIXME: temp hack
+    COLMAP="colmap"
 elif platform == "darwin":
     raise RuntimeError("Apple should not be legal")
 elif platform == "win32":
