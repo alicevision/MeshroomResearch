b67bb0c38276314293630133740b0f12c3eb3a56
Matthieu Hog <matthieu.hog@mikrosimage.com>
2023-09-01 14:07:06 +0200

    reverte changes to dataset and tests on meta write

--- mrrs/core/ios.py
+++ mrrs/core/ios.py
@@ -81,10 +81,15 @@ def save_exr(input_array, output_file, data_type='RGB',#FIXME: ugly
             for key in custom_header.keys():
                 # print("Writting metadata "+key)
                 # print(custom_header[key])
-                if key=="AliceVision:CArr":#FIXME: not working? need to make sure the arrays are list of float32
-                    spec.attribute(key, "float[3]", np.asarray(custom_header[key], dtype=np.float32).tolist())
+                # if key=="AliceVision:CArr":#FIXME: not working? need to make sure the arrays are list of float32
+                #     spec.attribute(key, "float[3]", np.asarray(custom_header[key], dtype=np.float32).tolist())
+                # elif key=="AliceVision:iCamArr":
+                #     spec.attribute(key, oiio.TypeDesc.TypeMatrix33,  np.asarray(custom_header[key], 
+                #                                                       dtype=np.float64).flatten().tolist())
+                if key=="AliceVision:CArr":#FIXME: not working, but was before...
+                    spec.attribute(key, "float[3]", list(custom_header[key]))
                 elif key=="AliceVision:iCamArr":
-                    spec.attribute(key, oiio.TypeDesc.TypeMatrix33,  np.asarray(custom_header[key], dtype=np.float32).flatten().tolist())
+                    spec.attribute(key, oiio.TypeDesc.TypeMatrix33,  list(custom_header[key].flatten()))
                 elif key=="AliceVision:downscale":
                     spec.attribute(key, "float", custom_header[key])
                 else:
@@ -451,6 +456,72 @@ def open_mesh(mesh_path):#FIXME: add suport to uv, texture etc
     mesh = trimesh.load(mesh_path)
     mesh = [mesh.vertices, mesh.faces ]
     return mesh
+
+
+def open_txt_calibration(calib_file, dataset="blendedMVG"):
+    """
+    Will open a calibration file in text format.
+    The format depends of the source and must be passed in parameters.
+    """
+    if dataset == "blendedMVG":
+        return open_txt_calibration_blendedMVG(calib_file)
+    # elif dataset == "ETH":
+    #     return open_txt_calibration_ETH(calib_file)
+    else:
+        raise RuntimeError("Unknown dataset")
+
+def open_txt_calibration_blendedMVG(calib_file):
+    '''
+    Opens a text file containing the calibration for a single camera, in the form :
+    extrinsic
+    E00 E01 E02 E03
+    E10 E11 E12 E13
+    E20 E21 E22 E23
+    E30 E31 E32 E33
+    intrinsic
+    K00 K01 K02
+    K10 K11 K12
+    K20 K21 K22
+    see https://github.com/YoYo000/MVSNet.
+    '''
+    intrinsics = np.zeros((3, 3))
+    extrinsics = np.zeros((4, 4))
+    with open(calib_file, "r") as calib_file:
+        words = calib_file.read().split()
+        # read extrinsic
+        for i in range(0, 4):
+            for j in range(0, 4):
+                extrinsic_index = 4 * i + j + 1
+                extrinsics[i][j] = words[extrinsic_index]
+        # read intrinsic
+        for i in range(0, 3):
+            for j in range(0, 3):
+                intrinsic_index = 3 * i + j + 18
+                intrinsics[i][j] = words[intrinsic_index]
+
+        # #extra info about depth range (unsused for now)
+        # if len(words) == 29:
+        #     intrinsics[3][0] = words[27]
+        #     intrinsics[3][1] = float(words[28]) * interval_scale
+        #     intrinsics[3][2] = max_d
+        #     intrinsics[3][3] = intrinsics[3][0] + intrinsics[3][1] * intrinsics[3][2]
+        # elif len(words) == 30:
+        #     intrinsics[3][0] = words[27]
+        #     intrinsics[3][1] = float(words[28]) * interval_scale
+        #     intrinsics[3][2] = words[29]
+        #     intrinsics[3][3] = intrinsics[3][0] + intrinsics[3][1] * intrinsics[3][2]
+        # elif len(words) == 31:
+        #     intrinsics[3][0] = words[27]
+        #     intrinsics[3][1] = float(words[28]) * interval_scale
+        #     intrinsics[3][2] = words[29]
+        #     intrinsics[3][3] = words[30]
+        # else:
+        #     intrinsics[3][0] = 0
+        #     intrinsics[3][1] = 0
+        #     intrinsics[3][2] = 0
+        #     intrinsics[3][3] = 0
+
+    return extrinsics, intrinsics
 
 def save_obj(file, scene_points, scene_faces=None, points_colors=None):
     """
