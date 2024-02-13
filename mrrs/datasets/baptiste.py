dc33a24bbdf54d4d6c3da9674c8b821f03658f79
Matthieu Hog <matthieu.hog@mikrosimage.com>
2024-02-13 10:41:58 +0100

    fixed ordering by frame

--- mrrs/datasets/baptiste.py
+++ mrrs/datasets/baptiste.py
@@ -11,24 +11,26 @@ def open_dataset(sfm_data):
     Opens "minimal" dataset from IRIT (matlab)
     #renders from https://github.com/bbrument/lambertianRendering_v1
     """
+    #resort by filename (frame order not working)
+    sfm_data["views"]=sorted(sfm_data["views"], key=lambda v:os.path.basename(v["path"]))
+
     #get the root of the dataset
     image_path = os.path.dirname(sfm_data["views"][0]["path"])
     dataset_root_path = os.path.abspath(os.path.join(image_path, ".."))
 
     #GT calib from the .sfm
-    sfm_folder=os.path.join(image_path, "..", 'sfm.json')
-    with open(sfm_folder, "r") as json_file:
-        sfm_data = json.load(json_file)
-    sfm_names_image_names =[os.path.basename(v["path"]) for v in sfm_data["views"]]
-    (extrinsics, intrinsics, _, _, _, _) = matrices_from_sfm_data(sfm_data)
+    gt_sfm_file=os.path.join(image_path, "..", 'sfm.json')
+    with open(gt_sfm_file, "r") as json_file:
+        gt_sfm_data = json.load(json_file)
+    (extrinsics, intrinsics, views_id, poses_id, intrinsics_id, pixel_sizes_all_cams) = matrices_from_sfm_data(gt_sfm_data)
     sensor_size = float(sfm_data["intrinsics"][0]["sensorWidth"])
 
     #images (sorted as in gt sfm)
-    image_names = [f for f in sorted(os.listdir(image_path)) if f.endswith(".png") ]
+    image_names = [os.path.basename(v["path"]) for v in gt_sfm_data["views"]] #[f for f in sorted(os.listdir(image_path)) if f.endswith(".png") ]
     images_sizes = [Image.open(os.path.join(image_path, image)).size for image in image_names]
 
     #transforms in mr format
-    extrinsics = [np.linalg.inv(np.concatenate( [e,[[0,0,0,1]]] )) for e in extrinsics]
+    # extrinsics = [np.linalg.inv(np.concatenate( [e,[[0,0,0,1]]] )) for e in extrinsics]
     pixel_size = sensor_size/images_sizes[0][0]
 
     #turn focal and pp into pixels for export
@@ -43,6 +45,11 @@ def open_dataset(sfm_data):
     #load mask names (sorted as in gt sfm)
     mask_map_path = os.path.abspath(os.path.join(dataset_root_path, "mask"))
     masks = sorted([os.path.join(mask_map_path, f) for f in os.listdir(mask_map_path) if f.endswith(".png") ])
+
+    sfm_names_image_names =[os.path.basename(v["path"]) for v in sfm_data["views"]]
+    print(image_names)
+    print(sfm_names_image_names)
+    #sanity check
 
     #sfm_names_image_names
     return {
