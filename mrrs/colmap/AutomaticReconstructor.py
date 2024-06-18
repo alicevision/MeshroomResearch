dea777cf946fda33ad5c14b3a68220d01f58857f
Matthieu Hog <matthieu.hog@mikrosimage.com>
2024-06-18 11:49:35 +0200

    cleanup colmap and blender

--- mrrs/colmap/AutomaticReconstructor.py
+++ mrrs/colmap/AutomaticReconstructor.py
@@ -1,7 +1,6 @@
 __version__ = "4.0"
 
 import os
-from sys import platform
 
 from meshroom.core import desc
 from . import COLMAP
@@ -20,7 +19,6 @@ class ColmapAutomaticReconstructor(desc.CommandLineNode):
             value='',
             uid=[0],
         ),
-
     ]
 
     outputs = [
